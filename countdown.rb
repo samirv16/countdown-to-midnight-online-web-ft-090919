1b454b1dbffd302c88645167c84ddfad690c346c
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-11 18:28:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,13 @@
 
 def countdown(number)
   while number > 0
+<<<<<<< HEAD
   puts "#{number} SECOND(S)!"
   number -= 1 
+=======
+  number -= 1 
+  puts "#{number} SECOND(S)!"
+>>>>>>> c942d6ead5fff2ce6d4e1c297036f4d6b69f6f80
   end
   "HAPPY NEW YEAR!"
 end
